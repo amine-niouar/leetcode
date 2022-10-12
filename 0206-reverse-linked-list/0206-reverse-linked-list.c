b74a4c11f821da09078424d209fa752b7cf2ccca
amine-niouar <57797343+amine-niouar@users.noreply.github.com>
2022-10-12 16:31:39 +0100

    Time: 2 ms (87.84%), Space: 6.5 MB (14.34%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.c
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -6,6 +6,73 @@
  * };
  */
 
+/*
+    int count(struct ListNode* head)
+{
+    struct ListNode* current;
+    int i;
+    
+    i = 0;
+    current = head;
+    while(current)
+    {
+        i++;
+        current = current->next;
+    }
+    return (i);
+}
+
+struct ListNode* get_node(struct ListNode* head, int pos)
+{
+    struct ListNode* current;
+    int i;
+    
+    i = 0;
+    current = head;
+     while(current)
+    {
+        if(pos == i)
+            return (current);
+        i++;
+        current = current->next;
+    }
+    return (0);
+}
+
+void swap(int *a, int *b)
+{
+    int c;
+    
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+struct ListNode* reverseList(struct ListNode* head){
+    
+    int i;
+    struct ListNode* current;
+     struct ListNode* currentPos;
+    int size;
+    int rev;
+    
+    current = head;
+    size = count(head);
+    rev = size - 1;
+    i = 0;
+    while (i < (size / 2))
+    {
+        currentPos = get_node(head,rev);
+        swap(&currentPos->val,&current->val);
+        rev--;
+        i++;
+        current = current->next;
+    }
+    return (head);
+}
+*/
+// Optimized solution
+
 struct ListNode* reverseList(struct ListNode* head){
     
     struct ListNode* prev,*current,*next;
@@ -14,6 +81,7 @@ struct ListNode* reverseList(struct ListNode* head){
     current = head;
     while(current)
     {
+        // current = 1 next = 2 current->next = NULL prev = 1 current = 2
         next = current->next;
         current->next = prev;
         prev = current;
